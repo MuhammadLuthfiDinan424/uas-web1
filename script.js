fefbd11baabca580c03ec89702fc1a147031b296
MuhammadLuthfiDinan424 <jjkggwp4@gmail.com>
2025-01-08 14:37:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function searchBooks() {
+  document.location = "https://tokopedia.link/UrnW4pvxYPb";
+}
+
+function searchBooks2() {
+  document.location = "https://tokopedia.link/Q6D5s8myYPb ";
+}
+
+function searchBooks3() {
+  document.location = "https://tokopedia.link/Ncu9kWvyYPb ";
+}
+
+function searchBooks4() {
+  document.location = "https://tokopedia.link/bZYb4MtyYPb ";
+}
+
+function searchBooks5() {
+  document.location = "https://tokopedia.link/zF1ZN9DyYPb ";
+}
+
+function searchBooks6() {
+  document.location = "https://tokopedia.link/eKDrbTxyYPb ";
+}
+
+function searchBooks7() {
+  document.location = "https://tokopedia.link/lZ4xfTzyYPb ";
+}
+
+function searchBooks8() {
+  document.location = " https://tokopedia.link/oXzynGWyYPb ";
+}
